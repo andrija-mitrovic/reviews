aec93ff383f0ba0e0bd0c7106d8b048951ad7ed4
Andrija Mitrovic <andrija.mitrovic@bild-studio.net>
2022-01-25 20:29:11 +0100

    Configure app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Review from "./components/Review";
+import data from "./data";
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
